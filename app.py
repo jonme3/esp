3bff094181d75396f2d95ea8df934c5f6bacbe95
jonme3 <jonme3@hotmail.com>
2025-01-08 13:02:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Esperando conexión..."  # Puedes devolver algo más específico si deseas.
+    return render_template('index.html')  # Servir el archivo HTML cuando se acceda a la raíz
 
 @app.route('/esp32_connected', methods=['GET'])
 def esp32_connected():
-    return jsonify({"status": "Conectado"}), 200  # Respuesta a la conexión del ESP32
+    # Aquí estamos respondiendo a las solicitudes del ESP32
+    return jsonify({"status": "Conectado"}), 200  # Responder que el ESP32 está conectado
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
